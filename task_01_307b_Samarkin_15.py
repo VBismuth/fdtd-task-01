3317da67be2b7a8ea56e786513dbc77402c580d9
Nik Sam <work.nicitons@yandex.ru>
2020-09-14 23:06:32 +0300

    Slightly change import

--- task_01_307b_Samarkin_15.py
+++ task_01_307b_Samarkin_15.py
@@ -1,7 +1,7 @@
 from pathlib import Path as pth
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
-import math as m
+from math import cos as cos
 
 #Переменные по заданию 15
 A = -0.25
@@ -12,8 +12,8 @@ def f(x):
   Sum1 = 0
   Sum2 = 0
   for i in range(1,6):
-    Sum1 += i * m.cos((i + 1)* x + i)
-    Sum2 += i * m.cos((i + 1)* A + i)
+    Sum1 += i * cos((i + 1)* x + i)
+    Sum2 += i * cos((i + 1)* A + i)
   return Sum1 * Sum2
 
 y = [f(_x) for _x in x]
